7391873fa9590a58483670ae0653b8f9a6c9706d
Camilo Torres <racknarow1@gmail.com>
2022-06-29 23:00:46 -0500

    Add Styled components

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,65 @@
 import { useState } from 'react'
+import styled from '@emotion/styled'
+import ImagenCripto from './img/imagen-criptos.png'
+import Formulario from './components/Formulario'
+
+
+// Styled components
+
+const Contenedor = styled.div`
+  max-width: 900px;
+  margin: 0 auto;
+  width: 90%;
+  @media (min-width: 992px) {
+    display: grid;
+    grid-template-columns: repeat(2, 1fr);
+    column-gap: 2rem;
+  }
+  `
+
+const Imagen = styled.img`
+  max-width: 400px;
+  width: 80%;
+  margin: 100px auto 0 auto;
+  display: block;
+  `
+
+const Heading = styled.h1`
+  font-family: 'Lato', sans-serif;
+  color: #fff;
+  text-align: center;
+  font-weight: 700;
+  margin-top: 80px;
+  margin-bottom: 50px;
+  font-size: 34px;
+
+  &::after {
+    content: '';
+    width: 100px;
+    height: 6px;
+    background-color: #66A2FE;
+    display: block;
+    margin: 10px auto 0 auto;
+  }
+  `
+
 
 
 
 function App() {
-  const [count, setCount] = useState(0)
+
 
   return (
-    <h1>Desde app</h1>
+    <Contenedor>
+      <Imagen 
+        src={ImagenCripto}
+        alt="Imagen de Criptomonedas"
+      />
+      <div>
+        <Heading>Cotiza Cryptomonedas al Instante</Heading>
+        <Formulario />
+      </div>
+    </Contenedor>
   )
 }
 
